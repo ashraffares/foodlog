5496de845748effab4a2856d000385acba8a0169
ashraffares <ashraffares090@gmail.com>
2021-04-11 18:21:05 +0200

    Push migration to schema

--- db/migrate/20210411161032_entry.rb
+++
@@ -1,5 +0,0 @@
-class Entry < ActiveRecord::Migration[6.1]
-  def change
-    t.references :user, null: false, foreign_key: true,index:true
-  end
-end
---
+++ db/migrate/20210411161931_add_user_id_to_entry.rb
@@ -0,0 +1,5 @@
+class AddUserIdToEntry < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :entries, :user, null: false, foreign_key: true
+  end
+end
